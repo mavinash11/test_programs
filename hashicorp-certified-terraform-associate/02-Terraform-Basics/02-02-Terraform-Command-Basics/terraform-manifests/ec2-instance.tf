e56efa6db8396f8f0c1b40f698648ae5e7f05b18
mavinash11 <102470258+mavinash11@users.noreply.github.com>
2025-09-18 18:36:02 +0530

    Update ec2-instance.tf
    Updated file ec2-instance.tf

--- hashicorp-certified-terraform-associate/02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ hashicorp-certified-terraform-associate/02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -11,11 +11,16 @@ terraform {
 # Provider Block
 provider "aws" {
   profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
-  region  = "us-east-1"
+  region  = "ap-south-1"
 }
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
-  instance_type = "t2.micro"
+  ami           = "ami-0d0ad8bb301edb745" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t3.micro"
+  key_name      = "MyKey" 
+  vpc_security_group_ids =  ["sg-08243a0b95b8cc267"]
+  tags = {
+    Name = "Configured via TF"
+    }
 }
